104d0c36c552be345191fd1fc1b6b0e29107809c
Carlos <cabelalcazar@bellezaexpress.com.co>
2017-03-31 10:16:18 -0500

    agrego nuevamente la carpeta

---
+++ resources/views/importacionesv2/importacionTemplate/createajax.blade.php
@@ -0,0 +1,47 @@
+@extends('importacionesv2.importacionTemplate.titulosbase')
+@section('generic')
+<!-- /**
+ * Template creado por
+ * Creado por Carlos Belalcazar
+ * Analista desarrollador de software Belleza Express
+ * 22/02/2017
+ */ -->
+<br><br><br>
+<div class="form-group">
+<label for="">Descripcion del producto:</label>
+<br>
+<label id="ref_producto"></label></div>
+@foreach($errors->all() as $key => $value)
+<div class="alert alert-danger">{{$value}}</div>
+@endforeach
+{{ Form::open(array('url' => "$route", 'id' => "formajax")) }}
+@foreach($campos as $key => $value )
+@if($value[2] == 'hidden')
+{{ Form::hidden("$value[0]") }}
+@elseif($value[2] == 'text')
+<div class="form-group">
+    {{ Form::label('', "$value[3]") }}
+    {{ Form::text("$value[0]", old("value[0]"), array('class' => 'form-control', 'id' => "$value[0]")) }}
+</div>
+
+@elseif($value[2] == 'select')
+<div class="form-group">
+    <label>{{$value[3]}}</label>
+    {{  Form::select($value[0],$value[5], null, ['placeholder'=>$value[4], 'class' => 'form-control'] ) }}
+</div>
+@elseif($value[2] == 'checkbox')
+<div class="mt-checkbox-list">
+    <label class="mt-checkbox mt-checkbox-outline">{{$value[3]}}
+        {{ Form::checkbox("$value[0]", '1') }}
+        <span></span>
+    </label>
+</div>
+@endif
+@endforeach
+
+
+<input type="text" style='display: none;' disabled="disabled" size="1">
+<input type="button" class="btn btn-primary" value="Crear Nueva" onclick="storeajax(document.getElementById('formajax').action, $('#formajax').serialize());">
+{{ Form::close() }}
+{!! $validator  !!}
+@endsection
---
+++ resources/views/importacionesv2/importacionTemplate/showImportacion.blade.php
@@ -0,0 +1,355 @@
+@extends('importacionesv2.importacionTemplate.titulosbase')
+@section('generic')
+<!-- ************************************ -->
+<!-- /************************************
+ * Template creado por
+ * Creado por Carlos Belalcazar
+ * Analista desarrollador de software Belleza Express
+ * 22/02/2017
+ *****************************************
+ *****************************************/ -->
+
+
+ <!-- ************************************ -->
+ <!-- General errors in form -->
+ <!-- ************************************ -->
+
+ <div class="form-group">
+   @if ($errors->all())
+   <div class="alert alert-danger" id="mensajealerta">
+    @foreach($errors->all() as $key => $value)
+    <span class="glyphicon glyphicon-remove red"></span>  {{$value}} <br>
+    @endforeach
+  @endif
+  @if (Session::has('message'))
+  <div class="alert alert-info">{{ Session::get('message') }}</div>
+  @endif
+</div>
+<!-- ************************************ -->
+<!-- End General errors in form -->
+<!-- ************************************ -->
+<div class="panel panel-primary">
+  <div class="panel-heading">Importacion:   {{$object->imp_consecutivo}} </div>
+  <div class="panel-body">
+
+    <div class="row"> 
+      <div class="col-sm-3">
+        <label  class="control-label"><strong>Razon social proveedor: </strong>{{$object->proveedor->razonSocialTercero}}</label>
+      </div>  
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>Nit proveedor: </strong>{{$object->proveedor->nitTercero}}</label>
+      </div> 
+      <div class="col-sm-3">
+        <label  class="control-label"><strong>Puerto de embarque: </strong>{{$object->puerto_embarque->puem_nombre}}</label>
+      </div> 
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>inconterm: </strong><br>{{$object->inconterm->inco_descripcion}}</label>
+      </div> 
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>Moneda negociación: </strong>{{$object->imp_moneda_negociacion}}</label>
+      </div> 
+
+    </div>
+    <br>
+
+    <div class="row"> 
+
+      <div class="col-sm-3">
+        <label  class="control-label"><strong>Fecha entrega total: </strong>{{$object->imp_fecha_entrega_total}}</label>
+      </div> 
+      <div class="col-sm-3">
+        <label  class="control-label"><strong>Observaciones: </strong>{{$object->imp_observaciones}}</label>
+      </div>  
+      <div class="col-sm-3">
+        <label  class="control-label"><strong>Estado: </strong><br>{{$object->estado->est_nombre}}</label>
+      </div>
+      <div class="col-sm-3">
+        <label  class="control-label"><strong>Origenes: </strong>
+          <ul class="list-group">
+            @foreach($objeto2 as $key => $value)
+            @if($value->origenes[0]->ormer_requ_cert_origen == 1)
+            <li class="">{{$value->origenes[0]->ormer_nombre}} Req. Certificado origen: SI</li>
+            @elseif($value->origenes[0]->ormer_requ_cert_origen == 0)
+            <li class="">{{$value->origenes[0]->ormer_nombre}} Req. Certificado origen: NO</li>
+            @endif
+            @endforeach
+          </ul>
+        </label>
+      </div>  
+
+    </div>
+    <div class="row">
+     <div class="col-sm-12">
+      <label  class="control-label"><strong>Proformas: </strong>
+        <ul class="list-group">
+          @foreach($objeto4 as $key => $value)
+          <pre><li class="">{{$value->prof_numero}} - F.creacion:{{\Carbon\Carbon::parse($value->prof_fecha_creacion)->format('d-m-Y')}} - F.entrega: {{\Carbon\Carbon::parse($value->prof_fecha_entrega)->format('d-m-Y')}} - Valor: {{$value->prof_valor_proforma}} - Principal: {{$value->prof_principal}}</li></pre>
+          @endforeach
+        </ul>
+      </label>
+    </div>  
+    <div class="col-sm-12">
+      <label  class="control-label"><strong>Productos: </strong>
+        <ul class="list-group">
+          @foreach($tablaProductos as $key => $value)
+          <pre>  <li class="">{{$value[0]}}</li></pre>
+          @endforeach
+        </ul>
+      </label>
+    </div>  
+  </div>
+
+</div>
+</div>
+
+
+<div class="panel panel-primary">
+  <div class="panel-heading">Embarque:  {{$object->imp_consecutivo}}</div>
+  <div class="panel-body">
+    <div class="row"> 
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>Embarcador: </strong>{{$objeto5[0]->embarcador->razonSocialTercero}} -- {{$objeto5[0]->embarcador->nitTercero}}</label>
+      </div>   
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>Linea maritima: </strong>{{$objeto5[0]->lineamaritima->razonSocialTercero}} -- {{$objeto5[0]->lineamaritima->lmar_descripcion}}</label>
+      </div>    
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>Tipo carga: </strong><br>{{$objeto5[0]->tipoCarga->tcar_descripcion}}</label>
+      </div>
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>Fecha ETD: </strong><br>{{\Carbon\Carbon::parse($objeto5[0]->emim_fecha_etd)->format('d-m-Y')}}</label>
+      </div>    
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>Fecha ETA: </strong><br>{{\Carbon\Carbon::parse($objeto5[0]->emim_fecha_eta)->format('d-m-Y')}}</label>
+      </div>      
+      <div class="col-sm-2">
+        <label  class="control-label"><strong>No. documento transporte: </strong><br>{{$objeto5[0]->emim_documento_transporte}}</label>
+      </div>    
+
+    </div>
+    <br>
+    <div class="row"> 
+     <div class="col-sm-2">
+      <label  class="control-label"><strong>Valor flete: </strong><br>{{$objeto5[0]->emim_valor_flete}}</label>
+    </div>    
+
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha recibido documentos originales: </strong><br>{{\Carbon\Carbon::parse($objeto5[0]->emim_fecha_recibido_documentos_ori)->format('d-m-Y') }}</label>
+    </div>   
+
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha de envio a la aduana: </strong><br>{{\Carbon\Carbon::parse($objeto5[0]->emim_fecha_envio_aduana)->format('d-m-Y') }}</label>
+    </div>   
+
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha de envio ficha tecnica: </strong><br>{{\Carbon\Carbon::parse($objeto5[0]->emim_fecha_envio_ficha_tecnica)->format('d-m-Y') }}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Aduana: </strong><br>{{$objeto5[0]->aduana->razonSocialTercero}} -- {{$objeto5[0]->aduana->nitTercero}}</label>
+    </div> 
+
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Transportador: </strong><br>{{$objeto5[0]->transportador->razonSocialTercero}} -- {{$objeto5[0]->transportador->nitTercero}}</label>
+    </div> 
+  </div>
+  <br>
+
+  <div class="row"> 
+   <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha de solicitud de la reserva: </strong><br>{{\Carbon\Carbon::parse($objeto5[0]->emim_fecha_solicitud_reserva)->format('d-m-Y')}}</label>
+  </div> 
+  <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha de confirmacion de la reserva: </strong><br>{{\Carbon\Carbon::parse($objeto5[0]->emim_fecha_confirm_reserva)->format('d-m-Y') }}</label>
+  </div> 
+  @if($objeto5[0]->contenedor[0]->cont_cajas == null)
+  <div class="col-sm-12">
+  <br>
+    <label  class="control-label"><strong>Contenedores: </strong>
+      <ul class="list-group">
+        @foreach($objeto5[0]->contenedor as $key => $value)
+        <pre><li class="">Tipo Contenedor {{$value->tipo->tcont_descripcion}} - Cantidad:{{$value->cont_cantidad}} - Numero: {{$value->cont_numero_contenedor}} - Cubicaje: {{$value->cont_cubicaje}} - Peso: {{$value->cont_peso}}</li></pre>
+        @endforeach
+      </ul>
+    </label>
+  </div>  
+  @elseif($objeto5[0]->contenedor[0]->cont_cajas != null)
+  <div class="col-sm-6">
+    <label  class="control-label"><strong></strong>
+      <ul class="list-group">
+        @foreach($objeto5[0]->contenedor as $key => $value)
+        <li class=""> Cubicaje: {{$value->cont_cubicaje}} - Peso: {{$value->cont_peso}} - Cajas: {{$value->cont_cajas}}</li>
+        @endforeach
+      </ul>
+    </label>
+  </div>  
+  @endif
+</div>
+
+<br>
+</div>
+</div>
+
+
+
+<div class="panel panel-primary">
+  <div class="panel-heading">Pagos:  {{$object->imp_consecutivo}}</div>
+  <div class="panel-body">
+
+   <div class="row"> 
+     <div class="col-sm-2">
+      <label  class="control-label"><strong>Valor anticipo: </strong><br>{{$objeto6[0]->pag_valor_anticipo}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha del anticipo: </strong><br>{{\Carbon\Carbon::parse($objeto6[0]->pag_fecha_anticipo)->format('d-m-Y')}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Valor saldo: </strong><br>{{$objeto6[0]->pag_valor_saldo}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha del saldo: </strong><br>{{\Carbon\Carbon::parse($objeto6[0]->pag_fecha_saldo)->format('d-m-Y')}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Valor comision: </strong><br>{{$objeto6[0]->pag_valor_comision}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Valor total: </strong><br>{{$objeto6[0]->pag_valor_total}}</label>
+    </div> 
+  </div>
+  <br>
+  <div class="row"> 
+
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Valor FOB: </strong><br>{{$objeto6[0]->pag_valor_fob}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>No. factura: </strong><br>{{$objeto6[0]->pag_numero_factura}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha factura: </strong><br>{{\Carbon\Carbon::parse($objeto6[0]->pag_fecha_factura)->format('d-m-Y')}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Trm liquidacion factura: </strong><br>{{$objeto6[0]->trm_liquidacion_factura}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha envio a contabilidad: </strong><br>{{\Carbon\Carbon::parse($objeto6[0]->pag_fecha_envio_contabilidad)->format('d-m-Y')}}</label>
+    </div> 
+  </div>
+
+</div>
+</div>
+
+<div class="panel panel-primary">
+  <div class="panel-heading">Nacionalizacion y costeo:  {{$object->imp_consecutivo}}</div>
+  <div class="panel-body">
+
+   <div class="row"> 
+     <div class="col-sm-2">
+      <label  class="control-label"><strong>Anticipo aduana: </strong><br>{{$objeto7[0]->naco_anticipo_aduana}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha anticipo anticipo aduana: </strong><br>{{$objeto7[0]->naco_fecha_anticipo_aduana}}</label>
+    </div> 
+    <div class="col-sm-2">
+      @if($objeto7[0]->naco_preinscripcion == 1)
+      <label  class="control-label"><strong>Preinscripcion: </strong><br>SI</label>
+      @elseif($objeto7[0]->naco_preinscripcion == 0)
+      <label  class="control-label"><strong>Preinscripcion: </strong><br>NO</label>
+      @endif
+
+    </div> 
+    <div class="col-sm-2">
+      @if($objeto7[0]->naco_control_posterior == 1)
+      <label  class="control-label"><strong>Control posterior: </strong><br>SI</label>
+      @elseif($objeto7[0]->naco_control_posterior == 0)
+      <label  class="control-label"><strong>Control posterior: </strong><br>NO</label>
+      @endif
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha recibo factura Belleza Express: </strong><br>{{$objeto7[0]->naco_fecha_recibo_fact_be}}</label>
+    </div> 
+    <div class="col-sm-2">
+      <label  class="control-label"><strong>Fecha entrega factura a contabilidad: </strong><br>{{$objeto7[0]->naco_fecha_entrega_fact_cont}}</label>
+    </div> 
+  </div>
+  <br>
+
+
+  <div class="row"> 
+   <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha entrega documentos transportador: </strong><br>{{$objeto7[0]->naco_fecha_entrega_docu_transp}}</label>
+  </div> 
+  <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha retiro puerto: </strong><br>{{$objeto7[0]->naco_fecha_retiro_puert}}</label>
+  </div> 
+  <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha de envio comex: </strong><br>{{$objeto7[0]->naco_fecha_envio_comex}}</label>
+  </div> 
+  <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha de llegada a Belleza Express: </strong><br>{{$objeto7[0]->naco_fecha_llegada_be}}</label>
+  </div> 
+  <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha recepcion lista de empaque: </strong><br>{{$objeto7[0]->naco_fecha_recep_list_empaq}}</label>
+  </div> 
+  <div class="col-sm-2">
+    <label  class="control-label"><strong>Fecha envio liquidación y costeo: </strong><br>{{$objeto7[0]->naco_fecha_envi_liqu_costeo}}</label>
+  </div> 
+</div>
+<br>
+
+<div class="row"> 
+ <div class="col-sm-2">
+  <label  class="control-label"><strong>Fecha entrada al sistema: </strong><br>{{$objeto7[0]->naco_fecha_entrada_sistema}}</label>
+</div> 
+<div class="col-sm-2">
+  @if($objeto6[0]->naco_sobrante)
+  <label  class="control-label"><strong>Sobrante: </strong><br>{{$objeto7[0]->naco_sobrante}}</label>
+  @elseif($objeto6[0]->naco_faltante)
+  <label  class="control-label"><strong>Faltante: </strong><br>{{$objeto7[0]->naco_faltante}}</label>
+  @endif
+</div> 
+<div class="col-sm-2">
+  <label  class="control-label"><strong>Factor importacion porcentual: </strong><br>{{$objeto7[0]->naco_factor_dolar_porc}}%</label>
+</div> 
+<div class="col-sm-2">
+  <label  class="control-label"><strong>Factor total: </strong><br>{{$objeto7[0]->naco_factor_dolar_tasa}}</label>
+</div> 
+<div class="col-sm-2">
+  <label  class="control-label"><strong>Factor logistico en pesos: </strong><br>{{$objeto7[0]->naco_factor_logist_porc}}</label>
+</div> 
+<div class="col-sm-2">
+  <label  class="control-label"><strong>Factor logistico: </strong><br>{{$objeto7[0]->naco_factor_logist_tasa}}</label>
+</div> 
+</div>
+<br>
+
+<div class="row"> 
+ <div class="col-sm-2">
+  <label  class="control-label"><strong>Tipo nacionalización: </strong><br>{{$objeto7[0]->tiponacionalizacion->tnac_descripcion}}</label>
+</div> 
+<div class="col-sm-10">
+  <label  class="control-label"><strong>Declaración de importación: </strong>
+    <ul class="list-group">
+      @foreach($objeto7[0]->declaracion as $key => $value)
+      <li class="">No. declaracion: {{$value->decl_numero}} - Sticker: {{$value->decl_sticker}} - Arancel: {{$value->decl_arancel}} - Iva: {{$value->decl_iva}} - Valor: {{$value->decl_valor_otros}} - Trm: {{$value->decl_trm}} - Levante: {{$value->levanteDeclaracion->tlev_nombre}} - Fecha aceptacion: {{$value->decl_fecha_aceptacion}} - Fecha de levante: {{$value->decl_fecha_levante}} - Fecha legaliza giro: {{$value->decl_fecha_legaliza_giro}} - Administracion dian: {{$value->admindianDeclaracion->descripcion}}</li>
+      @endforeach
+    </ul>
+  </label>
+</div>  
+
+</div>
+
+<br>
+
+</div>
+</div>
+@if($object->imp_estado_proceso != 6 && $hasPerm == 1)
+{{ Form::open(array('url' => route('cerrarImportacion'))) }}
+<input type="hidden" name="OrdenId" value="{{$object->id}}">
+<div class="form-group">
+  <button type="submit" class="btn btn-large btn-block btn-primary" onclick="">CERRAR ORDEN DE IMPORTACION</button>
+</div>
+{{ Form::close() }}
+@endif
+
+
+@endsection
